ab66c9285057a2c39cbc0db7e13770b9cbd38bff
Renzerth <thetrueordus@hotmail.com>
2015-02-28 15:00:29 -0500

    FIXED - Module call sequence

--- project_2014_numerical_methods/integrators/__init__.py
+++ project_2014_numerical_methods/integrators/__init__.py
@@ -1,5 +1,5 @@
 # integrators - initializing file
 # set of modules and submodules for numeric integration
 __all__ = ['adapquad','romberginteg','opennewtoncotes'];
-from integrators.adapquad import adapquad;
-from integrators.romberginteg import romberginteg;
+from adapquad import adapquad;
+from romberginteg import romberginteg;
--- project_2014_numerical_methods/integrators/opennewtoncotes/__init__.py
+++ project_2014_numerical_methods/integrators/opennewtoncotes/__init__.py
@@ -1,7 +1,7 @@
 # opennewtoncotes - submodule of integrators - initializing file
 # set of modules and submodules for numeric integration
 __all__ = ['midpointrule','onenodeopn','twonodeopn','thrnodeopn'];
-from integrators.opennewtoncotes.midpointrule import midpointrule;
-from integrators.opennewtoncotes.onenodeopn import onenodeopn;
-from integrators.opennewtoncotes.twonodeopn import twonodeopn;
-from integrators.opennewtoncotes.thrnodeopn import thrnodeopn;
+from midpointrule import midpointrule;
+from onenodeopn import onenodeopn;
+from twonodeopn import twonodeopn;
+from thrnodeopn import thrnodeopn;
