5c1c7715056e2046514a7534faa542b050f65463
Renzerth <thetrueordus@hotmail.com>
2015-04-28 11:26:38 -0500

    MOVED - This two functions were moved to a new directory: AdvanceIntegration

---
+++ project_2014_numerical_methods/Numericos/lib/integrators/AdvanceIntegration/__init__.py
@@ -0,0 +1,5 @@
+# AdvanceIntegration - initializing file
+# set of modules and submodules for numeric integration
+__all__ = ['adapquad','romberginteg.py'];
+from .adapquad import adapquad;
+from .romberginteg import romberginteg;
